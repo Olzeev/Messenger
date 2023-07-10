59daba84c9d4be8c8784df3fbd4504d36c4b43f0
Maxim <olzeevmax@gmail.com>
2023-07-10 16:35:18 +0300

    Chat list element changed

--- main/static/main/js/main.js
+++ main/static/main/js/main.js
@@ -10,9 +10,11 @@ $(document).ready(function() {
             success: function(response) {
                 $('#chat_list').empty()
                 for (let i = 0; i < response.users_found.length; i++){
-                    $("#chat_list").append('<li class="chat_list_element">' + response.users_found[i] + '</li>')
+                    $("#chat_list").append(`<li class="chat_list_element">
+                                                <img class="chat_list_element_image" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSshtD-7RtqN_oJMs3UfPKF7SQaUDHZjkuQoA&usqp=CAU">
+                                                <p class="chat_list_element_title">${response.users_found[i]}</p>
+                                            </li>`)
                 }
-                
             }
         });
     });
