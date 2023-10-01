86cab55a3920c5ebcb57b6c21d26946dbb70fc31
Maxim <olzeevmax@gmail.com>
2023-10-01 20:58:03 +0300

    change

--- main/static/main/js/main.js
+++ main/static/main/js/main.js
@@ -22,7 +22,7 @@ function show_chat_list(){
                                                 <p class="chat_list_element_title">${user[0]}</p>
                                                 <p class="chat_list_element_text">${user[1]}</p>
                                             </div>
-                                            <div style="width: 10px; height: 10px; background-color: red; visibility: "hidden""></div>
+                                            <div style="width: 10px; height: 10px; background-color: red; visibility: hidden"></div>
                                         </li>`
                 $("#chat_list").append(text)
                 chat_list.push([user[3], text])
