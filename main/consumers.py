4a5cdf3571d6d002a95310b2e44ed3045f314d7a
Maxim <olzeevmax@gmail.com>
2023-10-01 17:39:19 +0300

    change

--- main/consumers.py
+++ main/consumers.py
@@ -24,7 +24,7 @@ class GlobalConsumer(WebsocketConsumer):
         async_to_sync(self.channel_layer.group_send)(
             self.room_group_name, 
             {
-                'type': 'chat_message',
+                'type': 'chat',
                 'message': message, 
                 'sender': 0,  
                 'time': time
